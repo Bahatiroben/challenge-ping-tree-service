d59025b42a4de51b7bf4b3566bfd57459df2fc01
BAHATI Robben <39488016+Bahatiroben@users.noreply.github.com>
2021-06-11 23:11:01 +0200

    feat: get target by id (#3)
    upon passing the target id to a GET request,
    the server should return the target with that id

--- test/endpoints.js
+++ test/endpoints.js
@@ -60,6 +60,31 @@ test.serial.cb('get all targets', function (t) {
   })
 })
 
+test.serial.cb('get target by id', function (t) {
+  var url = '/api/target/1'
+  const redis = require('../lib/redis');
+  redis.hmset('targets', [1, JSON.stringify({id: 1, ...sampleData})])
+  servertest(server(), url, { encoding: 'json', method: 'GET' }, function (err, res) {
+    t.falsy(err, 'no error')
+    t.is(res.statusCode, 200, 'correct statusCode')
+    t.is(res.body.message, 'fetched target suceessfully', 'correct message returned')
+    t.is(res.body.data.id, 1, 'the returned target id matches the is in the url 1')
+    t.end()
+  })
+})
+
+test.serial.cb('returns 404 when it misses a target during get target by id', function (t) {
+  var url = '/api/target/x1_'
+  const redis = require('../lib/redis');
+  redis.hmset('targets', [1, JSON.stringify({id: 1, ...sampleData})])
+  servertest(server(), url, { encoding: 'json', method: 'GET' }, function (err, res) {
+    t.falsy(err, 'no error')
+    t.is(res.statusCode, 404, 'correct statusCode')
+    t.is(res.body.message, 'target not found', 'correct message returned')
+    t.end()
+  })
+})
+
 test.serial.cb('invalid method', function (t) {
   var url = '/api/targets'
   servertest(server(), url, { encoding: 'json', method: 'PUT' }, function (err, res) {
